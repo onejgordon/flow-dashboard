d90ddf974cdfe95a43fa37a81f1d65e035ee5dd5
Jeremy Gordon <onejgordon@gmail.com>
2017-08-18 15:20:08 -0700

    Update to new gcloud GAE directory

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module=${1:-all}
 
-sudo ./runtests.py /usr/local/google_appengine ../testing/ $module
+sudo ./runtests.py ~/google-cloud-sdk/platform/google_appengine ../testing/ $module
